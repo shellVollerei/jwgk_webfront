f4d53b5c9a350652b753bf2b778f9b5292264149
Siir <2485999859@qq.com>
2019-12-13 15:33:45 +0800

    add phone style of toollist

--- src/toolList/PhoneSiderNav.jsx
+++ src/toolList/PhoneSiderNav.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Icon } from "antd";
+import { Menu, Icon } from "antd";
+const { SubMenu } = Menu;
 // import QueueAnim from "rc-queue-anim";
 // import { getChildrenToRender } from './utils';
 
 class SiderNav extends React.PureComponent {
   closeCurtain = () => {
     console.log("close");
+    // TODO: 隐藏幕布 & phoneSiderNav
   };
   openCurtain = () => {
     console.log("open");
+    // TODO: 给幕布curtain和导航phoneSiderNav加闪出事件，并隐藏 Icon
   };
 
   render() {
@@ -16,7 +19,9 @@ class SiderNav extends React.PureComponent {
       <div>
         <div className="phone-sider-bar">
           <Icon
-            type="double-left"
+            type="database"
+            theme="filled"
+            className="right-icon"
             onClick={this.openCurtain}
             style={{
               fontSize: 40,
@@ -27,8 +32,64 @@ class SiderNav extends React.PureComponent {
             }}
           />
         </div>
-        <div className="nav-list-curtain" onClick={this.closeCurtain}>
-          <div className="nav-list"></div>
+        <div className="nav-list-curtain" onClick={this.closeCurtain}></div>
+        <div className="nav-list">
+          <Menu
+            onClick={this.handleClick}
+            style={{ width: 256 }}
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            mode="inline"
+          >
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="mail" />
+                  <span>Navigation One</span>
+                </span>
+              }
+            >
+              <Menu.ItemGroup key="g1" title="Item 1">
+                <Menu.Item key="1">Option 1</Menu.Item>
+                <Menu.Item key="2">Option 2</Menu.Item>
+              </Menu.ItemGroup>
+              <Menu.ItemGroup key="g2" title="Item 2">
+                <Menu.Item key="3">Option 3</Menu.Item>
+                <Menu.Item key="4">Option 4</Menu.Item>
+              </Menu.ItemGroup>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="appstore" />
+                  <span>Navigation Two</span>
+                </span>
+              }
+            >
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <SubMenu key="sub3" title="Submenu">
+                <Menu.Item key="7">Option 7</Menu.Item>
+                <Menu.Item key="8">Option 8</Menu.Item>
+              </SubMenu>
+            </SubMenu>
+            <SubMenu
+              key="sub4"
+              title={
+                <span>
+                  <Icon type="setting" />
+                  <span>Navigation Three</span>
+                </span>
+              }
+            >
+              <Menu.Item key="9">Option 9</Menu.Item>
+              <Menu.Item key="10">Option 10</Menu.Item>
+              <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item>
+            </SubMenu>
+          </Menu>
         </div>
       </div>
     );
