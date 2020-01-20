75495c5648602ab9e1311654a423fa5c11a6639f
Siir <2485999859@qq.com>
2020-01-20 20:55:50 +0800

    delete Home & Introduce

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * @SchoolStatus : 2016
  * @Date         : 2020-01-02 15:47:54
  * @LastEditors  : fatewang
- * @LastEditTime : 2020-01-07 21:47:42
+ * @LastEditTime : 2020-01-20 20:52:52
  * @Description  : Edit it for yourself
  * @ContactMe    : siir_52721@qq.com
  */
@@ -13,7 +13,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Router';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
